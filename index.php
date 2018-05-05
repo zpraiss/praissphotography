bb0ffc58dbb1059f7394a5a7785f02262733ce5a
Zach Praiss <zach.praiss@gmail.com>
2018-05-05 01:24:17 -0400

    Intro

---
+++ index.php
@@ -0,0 +1,58 @@
+<html>
+
+    <head>
+        <meta name="viewport" content="width=device-width, initial-scale=1.0" charset="utf-8">
+        <title>Zach Praiss</title>
+        <link rel="stylesheet" type="text/css" href="css/style.css">
+        <link rel="shortcut icon" href="favicon.png" type="image/x-icon">
+    </head>
+
+    <body>
+
+    	<?php include_once("php/analyticstracking.php") ?>
+
+        <div id="container">
+
+        	<a href="index.php" id="special1"><h1>Zach Praiss </h1></a>
+        	<a href="#info" id="special2"><h2>Info</h2></a>
+
+	        	<div class="hline"></div>
+
+				<a href="projects/workingfamilies.php"><img src=img/workingfamilies/image1.png class="fullfront"></a>
+	        	<h3>Protecting Working Families Tour</h3>
+	        	<a href="projects/workingfamilies.php"><h4>See full project ></h4></a>
+
+	        	<a href="projects/womensmarch.php"><img src=img/womensmarch/front.jpg class="fullfront"></a>
+	        	<h3>Women's March</h3>
+	        	<a href="projects/womensmarch.php"><h4>See full project ></h4></a>
+
+	        	<a href="projects/joebiden.php"><img src=img/joebiden/front.png class="fullfront"></a>
+	        	<h3>Joe Biden eats Cornell Dairy ice cream</h3>
+	        	<a href="projects/joebiden.php"><h4>See full project ></h4></a>
+
+	        	<a href="projects/iamamuslim.php"><img src=img/iamamuslim/front.jpg class="fullfront"></a>
+	        	<h3>I Am A Muslim Too</h3>
+	        	<a href="projects/iamamuslim.php"><h4>See full project ></h4></a>
+
+	        	<a href="projects/lifted.php"><img src=img/lifted/front.jpg class="fullfront"></a>
+	        	<h3>Lifted</h3>
+	        	<a href="projects/lifted.php"><h4>See full project ></h4></a>
+
+	        	<a href="projects/letsgetcoffee.php"><img src=img/letsgetcoffee/front.jpg class="fullfront"></a>
+	        	<h3>Let's Get Coffee</h3>
+	        	<a href="projects/letsgetcoffee.php"><h4>See full project ></h4></a>
+	        	
+	        	<a href="projects/bookwagon.php"><img src=img/bookwagon/front.jpg class="fullfront"></a>
+	        	<h3>The Book Wagon</h3>
+	        	<a href="projects/bookwagon.php"><h4>See full project ></h4></a>
+
+	        	<a href="projects/wordsofwisdom.php"><img src=img/wordsofwisdom/front.jpg class="fullfront"></a>
+	        	<h3>Words of Wisdom</h3>
+	        	<a href="projects/wordsofwisdom.php"><h4>See full project ></h4></a>
+
+			<?php include("php/footer.php"); ?>
+
+        </div>
+    </body>
+
+</html>
